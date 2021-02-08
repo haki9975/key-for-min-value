a8859c97ef941366d3561c8feb70a87aed300cd0
haki9975 <elias.robert.hakim@gmail.com>
2021-02-08 14:50:53 -0500

    commit tree 2900a997fab710cea07c7699961fcfd74a260308

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,19 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
-
+require 'pry'
 def key_for_min_value(name_hash)
-
+   
+  low_number = nil
+  key= nil
+    name_hash.collect do |name, number|
+      
+     # binding.pry 
+      if low_number == nil || low_number > number
+        low_number = number
+        key = name
+        
+      end 
+    end
+    
+    return key
 end
